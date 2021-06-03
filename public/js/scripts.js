4f4b0e7ff45bdfbe7ede887ec821cf5f0489a6e2
birdiedaboy <abraham.esparza.eb@gmail.com>
2021-06-02 23:28:13 -0500

    footer y menu

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,14 +1,14 @@
-window.addEventListener("click", function (e) {
-  if (!document.getElementById("actionLanguage").contains(e.target)) {
-    const element = document.getElementById("languages");
-    const classes = element.classList.value;
-    const isShowElement = classes.includes("showModalLanguage");
-    if (isShowElement) {
-      element.classList.remove("showModalLanguage");
-      return;
-    }
-  }
-});
+//window.addEventListener("click", function (e) {
+//  if (!document.getElementById("actionLanguage").contains(e.target)) {
+//    const element = document.getElementById("languages");
+//    const classes = element.classList.value;
+//    const isShowElement = classes.includes("showModalLanguage");
+//    if (isShowElement) {
+//      element.classList.remove("showModalLanguage");
+//      return;
+//    }
+//  }
+//});
 
 function changeCityInput() {
   const cityOrganization = document.getElementById("cityOrganization").value;
