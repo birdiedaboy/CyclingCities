5ff6178651d37140f6a296f2fb427b0b7d92244e
remi <ieddaguilar@gmail.com>
2021-06-03 22:58:10 -0500

    lenguajes

--- public/js/languages.js
+++ public/js/languages.js
@@ -1,8 +1,12 @@
 let languages = {
   en: {
     home_section_1_title: "How are you",
+    data_main_form_label_email: 'Email:',
+    data_password: 'Password',
   },
   es: {
     home_section_1_title: "Como estas tu",
+    data_main_form_label_email: 'Correo electronico: ',
+    data_password: 'Contrasena <strong>requerida<strong>',
   },
 };
